799f1ec1adf09a486836e349727d7d8be9956516
YYryurtu <sahulkyo@gmail.com>
2023-09-17 11:11:21 +0300

    sims hw

--- main1.py
+++ main1.py
@@ -24,9 +24,9 @@ brands_of_car = {
 class Human:
     def __init__(self,name="Human",job=None,home=None,car=None):
         self.name = name
-        self.money = 100
-        self.gladness = 50
-        self.satiety = 50
+        self.money = 120
+        self.gladness = 60
+        self.satiety = 60
         self.job = job
         self.car = car
         self.home = home
