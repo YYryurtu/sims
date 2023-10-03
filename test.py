4ae452b9aa83b0ecbfee8b7e67a8396fa27f48ed
YYryurtu <sahulkyo@gmail.com>
2023-10-03 12:58:31 +0300

    simstest.py

--- test.py
+++ test.py
@@ -6,6 +6,15 @@ class a (unittest.TestCase):
         b = Human()
         day = 0
         self.assertTrue(b.live(day=day))
+        self.assertTrue(b.shopping())
+        self.assertTrue(b.work())
+        self.assertTrue(b.eat())
+        self.assertTrue(b.get_job())
+        self.assertTrue(b.is_alive())
+        self.assertTrue(b.live())
+
+
+
 
     def test2(self):
         c = Auto()
